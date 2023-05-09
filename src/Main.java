0352254d38fd681ed6bd98b448ea35d1ea06de23
Segii Kolisnichenko <sk@etfkkntu.ukr.education>
2023-05-09 15:10:00 +0300

    Вирішив задачу про суму двох елементів у масиві.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 15;
-
         int[] ints = twoSum(nums, target);
         System.out.println(ints[0] + " " + ints[1]);
     }
