f860e4ee18fbbce42070f6c46f4f4d0ff3f2e59c
Segii Kolisnichenko <sk@etfkkntu.ukr.education>
2023-05-09 14:55:09 +0300

    Вирішив задачу про суму двох елементів у масиві.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that
+they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+        Example 1:
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].*/
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.println("Привіт!");
+        int[] nums = {2,7,11,15};
+        int target = 15;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int[] ints = twoSum(nums, target);
+        System.out.println(ints[0] + " " + ints[1]);
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target){
+//                    System.out.println(i + " " + j);
+                    answer[0] = i;
+                    answer[1] = j;
+                }
+            }
         }
+        return answer;
     }
 }
